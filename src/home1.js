c60005dacdaa9167c4b2f0838cbad85e587c3401
SuhasPalani <sugipals007@gmail.com>
2023-02-12 18:22:09 +0530

    first commit

---
+++ src/home1.js
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+const Home = () => {
+
+   // let name='suhas';
+
+   const [name,setName]= useState('Suhas');
+    const [age,setAge]= useState(25);
+
+    const handleClick = () => {
+        setName('Sugi');
+        setAge(30);
+    }
+    // const handleClickAgain = (name, e) => {
+    //     console.log('hello '+ name,e.target);
+    // }
+    return (  
+        
+        <div className="home">
+            <h2>HomePage</h2>
+            <p>{name} is {age} years old</p>
+            <button onClick={handleClick}>Click me</button>
+            {/* <button onClick={(e)=>handleClickAgain('Suhas',e)}>Click me again</button> */}
+        </div>
+    );
+}
+ 
+export default Home;
