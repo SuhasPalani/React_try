93029b7d54d07a686bb99d447a46311e2f6a58c6
SuhasPalani <sugipals007@gmail.com>
2023-02-14 17:43:24 +0530

    del commit

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,7 +1,8 @@
-const BlogList = ({blogs,title}) => {
+const BlogList = ({blogs,title,handleDelete}) => {
     // const blogs=props.blogs;
     // const title=props.title;
     // console.log(props,blogs);
+   
 
     return ( 
         <div className="blog-list">
@@ -10,6 +11,7 @@ const BlogList = ({blogs,title}) => {
                 <div className="blog-preview" key={blog.id}>
                     <h2>{blog.title}</h2>
                     <p>Written By {blog.author}</p>
+                    <button onClick={() => handleDelete(blog.id)}>Delete Blog</button>
                 </div>
             ))}
         </div>
